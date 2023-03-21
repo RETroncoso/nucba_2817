1119a059e66a20d1e17cfc3167564e37abf05796
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2023-03-20 21:44:57 -0300

    clase_01 JS

---
+++ Javascript/clase_01/main.js
@@ -0,0 +1,49 @@
+// Tipos de datos
+
+// - Number = 1/54/6.98/.6549
+// - Booleanos = true / false
+// - String = "Hola" / 'Hola' / `Hola`
+// - undefined = Algo no definido
+// - null = vacío
+
+//Variables
+var numeroA = 15;
+var numeroB = 20;
+
+console.log(numeroA + numeroB);
+
+var textoA = "Harry";
+var textoB = "Potter";
+
+console.log(textoA + " " + textoB);
+
+//Var
+console.log(tipoVar);
+var tipoVar = "Hola! Soy var";
+console.log(tipoVar);
+
+//Let
+let tipoLet = "Hola! Soy let";
+tipoLet = "Otro Ejemplo";
+console.log(tipoLet);
+
+//Const
+const tipoConst = "Hola! Soy const";
+// tipoConst = "Otro ejemplo";
+console.log(tipoConst);
+//bool, string, number, undefined
+
+//typeof
+console.log(typeof 5);
+console.log(typeof "5");
+console.log(typeof true);
+console.log(typeof 5.3);
+console.log(typeof NaN);
+
+//prompt();
+
+const namePrompt = prompt("Ingresa tu nombre:");
+
+console.log(`Hola ${namePrompt}!`);
+
+alert(`Hola ${namePrompt}!`);
